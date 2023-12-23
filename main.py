a177f7255129a0423b02a83dbffae2f5cef2beb1
AnuragDahal <freefirepros2020@gmail.com>
2023-12-23 09:38:50 +0545

    patch

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ from fastapi import FastAPI, APIRouter
 from router import user
 from core.database import engine
 from core import models
+from fastapi.middleware.cors import CORSMiddleware
 
+origins = [
+    "http://127.0.0.1:5500",
+
+]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 router = APIRouter()
 
 models.Base.metadata.create_all(engine)
