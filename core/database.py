e52576626359788c7a4e96a05df80eb2de6e67d9
AnuragDahal <freefirepros2020@gmail.com>
2023-12-25 12:38:03 +0545

    modified database file

--- core/database.py
+++ core/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 import os
 
-SQLALCHEMY_DATABASE_URL = os.environ[DATABASE_URL]
+load_dotenv()  # take environment variables from .env.
+
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
