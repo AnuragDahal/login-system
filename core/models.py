834c1d8e0b7a0ebe25a6f00174a852201b06a8a8
AnuragDahal <freefirepros2020@gmail.com>
2023-12-28 21:12:58 +0545

    added time

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from core.database import Base
+from datetime import datetime
 # from sqlalchemy.orm import relationship
 
 
@@ -10,3 +11,4 @@ class User(Base):
     Name = Column(String(100), unique=True, index=True)
     Email = Column(String(50), nullable=False, unique=True)
     password = Column(String(40), nullable=False)
+    time = Column(Integer, default=datetime.now())
