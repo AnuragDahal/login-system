31d87f5b9f8c58ace81a609b4ec4fdeaeca58c93
AnuragDahal <freefirepros2020@gmail.com>
2023-12-30 12:16:55 +0545

    hashed pass

--- core/database.py
+++ core/database.py
@@ -4,14 +4,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # take environment variables from .env.
+# load_dotenv()  # take environment variables from .env.
 
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 # Check if DATABASE_URL is None
 if SQLALCHEMY_DATABASE_URL is None:
     raise ValueError("DATABASE_URL is not set in the environment variables.")
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
+                       "check_same_thread": False})
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
