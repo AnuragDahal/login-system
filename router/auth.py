791a7f9809b784413fb8a71d63c6c25e93216f65
AnuragDahal <freefirepros2020@gmail.com>
2024-01-01 21:07:03 +0545

    jwt implementation

--- router/auth.py
+++ router/auth.py
@@ -1,7 +1,15 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from core import models, schemas, database
-from . import hash
+from . import hash, jwt_token
+from datetime import timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("Access_Token_Expires")
+
 
 get_db = database.get_db
 
@@ -24,4 +32,9 @@ async def login(request: schemas.Login, db: Session = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
 
-    return {"message": "Login successful", "user_id": user.id, "Name": user.Name}
+# GENERATE A JWT TOKEN AND RETURN IT
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = jwt_token.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
