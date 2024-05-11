bdeea3c67821246a53fea15bf10922e116c1be2a
AnuragDahal <freefirepros2020@gmail.com>
2024-05-11 18:07:55 +0545

    fixing typos

--- router/user.py
+++ router/user.py
@@ -46,7 +46,7 @@ async def put_details(name: str, address: str, age: int):
 
 @router.get("/cookie-login/check", status_code=status.HTTP_200_OK)
 async def cookie_login(req: Request, dependencies: Session = Depends(check_cookie)):
-    if dep:
+    if dependencies:
         return {"detail": "Cookie auhtenticated successfully"}
     else:
         raise HTTPException(
